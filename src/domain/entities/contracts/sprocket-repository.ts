5f63601f402cd7b9e96fd53224b9f67dbfed01f2
Pablo Giraldo <j.giraldo@chaione.com>
2024-06-10 09:10:34 -0500

    feat(interface): Defining getAllSprockets in ISprocketRepository interface repository

--- src/domain/entities/contracts/sprocket-repository.ts
+++ src/domain/entities/contracts/sprocket-repository.ts
@@ -1,5 +1,7 @@
+import { SprocketEntity } from '@/domain/entities/sprocket';
+
 export const SPROCKET_REPOSITORY = 'SPROCKET_REPOSITORY';
 
 export interface ISprocketRepository {
-    
+  getAllSprockets(): Promise<SprocketEntity[]>;
 }
