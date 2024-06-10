ce75a9079f6990514abefefd4bf86c747596ca3d
Pablo Giraldo <j.giraldo@chaione.com>
2024-06-10 12:25:31 -0500

    feat(service): Defining getFactoryById Service

--- src/domain/use-cases/factory-service.ts
+++ src/domain/use-cases/factory-service.ts
@@ -1,5 +1,12 @@
-export const FACTORY_SERVICES = 'FACTORY_SERVICES';
-        
-export interface IFactoryService {
+import { Service } from '@tsclean/core';
+import { FactoryEntity } from '@/domain/entities/factory';
+import { FactoryRepository } from '@/domain/entities/contracts/factory-repository';
 
+@Service()
+export class FactoryService {
+  constructor(private readonly factoryRepository: FactoryRepository) {}
+
+  async getFactoryById(id: string): Promise<FactoryEntity | null> {
+    return this.factoryRepository.getFactoryById(id);
+  }
 }
