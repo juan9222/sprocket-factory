7f989416a55c60df25b82250e2f2413696f64b5c
Pablo Giraldo <j.giraldo@chaione.com>
2024-06-10 12:27:41 -0500

    feat(entities): Defining Factory Entity

--- src/domain/entities/factory.ts
+++ src/domain/entities/factory.ts
@@ -1,5 +1,13 @@
 export type FactoryEntity = {
-    // Attributes
-}
+    id: string;
+    factory: {
+      chart_data: {
+        sprocket_production_actual: number[];
+        sprocket_production_goal: number[];
+        time: number[];
+      };
+    };
+  };
+  
 
 export type AddFactoryParams = Omit<FactoryEntity, 'id'>
